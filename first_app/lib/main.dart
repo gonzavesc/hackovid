d265738401a41640632bbe9e118ac1353912154d
Gonzalo <gonzalovescovi@gmail.com>
2020-04-09 20:16:06 +0200

    Using columns added buttons to scaffold and body

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questions = ["What is your favourite color", "What is your favourite animal"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("MyApp!"),
       ),
-      body: Text("This is my default body!"),
+      body: Column(children: <Widget>[Text("Thequestion"),
+      RaisedButton(child: Text("First option"),onPressed: null,),
+      RaisedButton(child: Text("Second option"), onPressed: null,)],),
     ));
   }
 }
