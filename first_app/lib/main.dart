d990220409f3690ca9c17be0e0f612dbd9ae0281
Gonzalo <gonzalovescovi@gmail.com>
2020-04-09 20:28:15 +0200

    Added function to button

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,6 +6,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion (){
+    print("Answer chosen");
+  }
   var questions = ["What is your favourite color", "What is your favourite animal"];
   @override
   Widget build(BuildContext context) {
@@ -15,8 +18,8 @@ class MyApp extends StatelessWidget {
         title: Text("MyApp!"),
       ),
       body: Column(children: <Widget>[Text("Thequestion"),
-      RaisedButton(child: Text("First option"),onPressed: null,),
-      RaisedButton(child: Text("Second option"), onPressed: null,)],),
+      RaisedButton(child: Text("First option"), onPressed: answerQuestion,),
+      RaisedButton(child: Text("Second option"), onPressed: answerQuestion,)],),
     ));
   }
 }
