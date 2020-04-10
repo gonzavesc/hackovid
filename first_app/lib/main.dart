7216d281cf6b27823c84a6c3387157d03ab78971
Gonzalo <gonzalovescovi@gmail.com>
2020-04-10 11:17:13 +0200

    Trying to change text with statelesswidget no good

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,7 +6,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
   void answerQuestion (){
+    questionIndex = questionIndex + 1;
     print("Answer chosen");
   }
   var questions = ["What is your favourite color", "What is your favourite animal"];
@@ -17,9 +19,9 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("MyApp!"),
       ),
-      body: Column(children: <Widget>[Text("Thequestion"),
+      body: Column(children: <Widget>[Text(questions.elementAt(questionIndex)),  //same asd questions[0]
       RaisedButton(child: Text("First option"), onPressed: answerQuestion,),
-      RaisedButton(child: Text("Second option"), onPressed: answerQuestion,)],),
+      RaisedButton(child: Text("Second option"), onPressed: () => print("Second chosen"),)],),
     ));
   }
 }
