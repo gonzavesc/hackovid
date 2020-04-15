1b93d1d9cc3725bc1d90e90239726630d9c0fc86
Gonzalo <gonzalovescovi@gmail.com>
2020-04-15 19:31:57 +0200

    Added button to restart quiz

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -49,6 +49,13 @@ class _MyAppState extends State<MyApp> {
     }
   ];
   var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore = _totalScore + score;
     print(_totalScore);
@@ -75,7 +82,7 @@ class _MyAppState extends State<MyApp> {
               questions: _questions,
               questionIndex: _questionIndex,
             )
-          : Result(_totalScore),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
