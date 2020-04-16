d5192beaeb963aed4db990a5e5b8895988c54fff
Gonzalo <gonzalovescovi@gmail.com>
2020-04-16 18:52:32 +0200

    Started expenses tracker app

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("CHART!"),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text("List of TX"),
+            )
+          ],
+        ));
+  }
+}
