631a63848e7712aa249ffa8ae9e8802ffffe8fda
Gonzalo <gonzalovescovi@gmail.com>
2020-04-18 11:35:02 +0200

    Added input text and button

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -33,7 +33,8 @@ class MyHomePage extends StatelessWidget {
           title: Text("Flutter App"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -41,6 +42,26 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text("CHART!"),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                    ),
+                    TextField(decoration: InputDecoration(labelText: "Amount")),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Text("Add Transaction"),
+                      textColor: Colors.purple,
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
@@ -66,7 +87,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text(
                             tx.title,
