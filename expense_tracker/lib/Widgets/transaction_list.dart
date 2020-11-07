22233c203cdbc507fca7587f65dd98add14355f2
Gonzalo <gonzalovescovi@gmail.com>
2020-11-07 17:48:27 +0100

    126, made icon with text when there is space

--- expense_tracker/lib/Widgets/transaction_list.dart
+++ expense_tracker/lib/Widgets/transaction_list.dart
@@ -10,23 +10,23 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx, constraints){
-            return Column(
-              children: <Widget>[
-                Text("No transactions added yet!",
-                    style: Theme.of(context).textTheme.title),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: constraints.maxHeight * 0.6,
-                    child: Image.asset(
-                      'assets/images/waiting.png',
-                      fit: BoxFit.cover,
-                    ))
-              ],
-            );
-          }) 
+          ? LayoutBuilder(builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text("No transactions added yet!",
+                      style: Theme.of(context).textTheme.title),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: constraints.maxHeight * 0.6,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      ))
+                ],
+              );
+            })
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
@@ -47,10 +47,16 @@ class TransactionList extends StatelessWidget {
                     ),
                     subtitle: Text(
                         DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => deleteTx(transactions[index].id),
-                        color: Theme.of(context).errorColor),
+                    trailing: MediaQuery.of(context).size.width > 360
+                        ? FlatButton.icon(
+                            onPressed: () => deleteTx(transactions[index].id),
+                            icon: Icon(Icons.delete),
+                            textColor: Theme.of(context).errorColor,
+                            label: Text('Delete'))
+                        : IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => deleteTx(transactions[index].id),
+                            color: Theme.of(context).errorColor),
                   ),
                 );
               },
